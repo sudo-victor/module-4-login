96a3efe613d5952377dcf7bdf3885b310db6fa9f
Victor de Souto <victor.souto@hotmail.com>
2023-01-10 21:14:16 -0300

    ajudando o thiago

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,7 @@
 import * as express from 'express'
 
-const thiago = express();
+const app = express();
 
-thiago.use(express.json());
+app.use(express.json());
 
-function PequiController(req, res) {
-  res.send('nininho')
-}
-
-function ValidadorPequi(req, res, next) {
-  const { token } = req.headers
-  // Validar meu token
-  res.status(401).json({ message: 'Invalid Token'})
-  return res.status(401).json({ message: 'Unauthorized'})
-  next()
-}
-
-thiago.get('/pequi', ValidadorPequi, ValidadorPequi, ValidadorPequi, PequiController)
-
-// Params
-// Query
-// Body
-// Header
-
-thiago.listen(3333, () => console.log('listening'))
+app.listen(3333, () => console.log('listening'))
